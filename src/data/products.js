1d3507000ca23b979ad9324cf69056cacdd6c5a9
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-08-18 23:16:35 +0330

    feat: add product data model
    - define centralized product data with slug, name, image, and descriptions

---
+++ src/data/products.js
@@ -0,0 +1,56 @@
+export const products = [
+  {
+    slug: 'supercopp',
+    name: 'SuperCOPP',
+    shortDescription: 'Organik fungisit ve bakterisid. Mantar-bakteriyel hastalıklarla mücadelede kimyasal bakır zehirlerine en iyi alternatif.',
+    image: '/images/products/product-supercopp.avif'
+  },
+  {
+    slug: 'potasgen',
+    name: 'Potasgen',
+    shortDescription: 'Yüksek organik potasyumlu sıvı gübre (NPK 8-0-15). Meyvenin büyüklüğünü ve kalitesini artırır.',
+    image: '/images/products/product-potasgen.avif'
+  },
+  {
+    slug: 'pf-nematod',
+    name: 'PF Nematod',
+    shortDescription: 'Nematod hastalığıyla mücadelede kökteki nematodu ve kök çevresindeki toprağı yok eder.',
+    image: '/images/products/product-pf-nematod.avif'
+  },
+  {
+    slug: 'pf-trips',
+    name: 'PF Trips',
+    shortDescription: 'Organik böcek ilacı. Thrips ile mücadelede en iyi organik seçenek.',
+    image: '/images/products/product-pf-trips.avif'
+  },
+  {
+    slug: 'hb-growth-n',
+    name: 'HB Growth N',
+    shortDescription: 'Organik yüksek nitrojenli NPK 26-0-0 sıvı gübre. Azot noksanlığı ve yeşillik sorununu çözer.',
+    image: '/images/products/product-hbgrown.avif'
+  },
+  {
+    slug: 'fulviazot',
+    name: 'Fulviazot',
+    shortDescription: 'Organik amino asit ilaveli, yüksek nitrojenli NPK 10-0-0 sıvı gübre.',
+    image: '/images/products/product-fulviazot.avif'
+  },
+  {
+    slug: 'calciboramin',
+    name: 'CalciBorAmin',
+    shortDescription: 'Kalsiyum, bor ve organik çinko içeren sıvı gübre. Yapraktan ilaçlama ve köklendirme yoluyla kullanılır.',
+    image: '/images/products/product-calciboramin.avif'
+  },
+  {
+    slug: 'bor-plus',
+    name: 'Bor Plus',
+    shortDescription: 'Organik bor içeren sıvı gübre. Çiçeklenme ve meyve tutumunu artırır.',
+    image: '/images/products/product-borblus.avif'
+  },
+  {
+    slug: 'amino-mangaland',
+    name: 'Amino Mangaland',
+    shortDescription: 'Organik manganez ve çinko içeren sıvı gübre. Orta yapraklardaki solma ve deformasyonları önler.',
+    image: '/images/products/product-aminomangaland.avif'
+  }
+];
